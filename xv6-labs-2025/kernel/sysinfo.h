dbf8f300d147116c75c744194864e74236502979
Tan Hiep Le <23120255@student.hcmus.edu.vn>
2025-11-07 21:58:25 +0700

    Add struct for sysinfo

---
+++ xv6-labs-2025/kernel/sysinfo.h
@@ -0,0 +1,5 @@
+struct sysinfo {
+  uint64 freemem;      // Amount of free memory (bytes)
+  uint64 nproc;        // Number of processes
+  uint64 nopenfiles;   // Number of open file descriptors
+};
