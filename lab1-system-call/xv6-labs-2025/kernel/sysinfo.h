9d582d853d8ff99c71550ac062bef143f2f9da61
Chuối <145743624+henry-banana@users.noreply.github.com>
2025-11-25 14:55:16 +0700

    first commit

--- lab1-system-call/xv6-labs-2025/kernel/sysinfo.h
+++
@@ -1,6 +0,0 @@
-// By Tan Hiep Le
-struct sysinfo {
-  uint64 freemem;      // Amount of free memory (bytes)
-  uint64 nproc;        // Number of processes
-  uint64 nopenfiles;   // Number of open file descriptors
-};
